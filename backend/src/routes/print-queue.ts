004fcf4f614d5ead54f13d0b2f7472abf60e64f7
Eirik Joneid <eirik.joneid@technipfmc.com>
2025-03-16 17:33:22 +0100

    feat(b): zod input validation

--- backend/src/routes/print-queue.ts
+++ backend/src/routes/print-queue.ts
@@ -1,5 +1,6 @@
 import type { BunRequest } from "bun";
 import {db} from "../db.ts";
+import {z} from "zod";
 
 export function getPrintQueue(req: BunRequest): Response {
     const selectPrintQueueStatement = db.query("SELECT * FROM print_queue");
@@ -7,9 +8,14 @@ export function getPrintQueue(req: BunRequest): Response {
     return Response.json(printQueue);
 }
 
+const PrintItemSchema = z.object({
+    name: z.string(),
+    url: z.string().nullable(),
+});
+
 export async function postPrintQueue(req: BunRequest): Promise<Response> {
-    const body = await req.json();
+    const body = PrintItemSchema.parse(await req.json());
     const insertPrintQueueStatement = db.query("INSERT INTO print_queue (name, url) VALUES ($name, $url) RETURNING *");
-    const created = insertPrintQueueStatement.get({name: body.name, url: body.url});
+    const created = insertPrintQueueStatement.get(body);
     return Response.json(created);
 }
