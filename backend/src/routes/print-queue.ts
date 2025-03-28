f4855759b7ef56f3e6bdf4b835f63b7320196e8c
Eirik Joneid <33673748+ejoneid@users.noreply.github.com>
2025-03-28 19:22:46 +0100

    feat(b,f): add authentication (#3)

--- backend/src/routes/print-queue.ts
+++ backend/src/routes/print-queue.ts
@@ -1,8 +1,10 @@
 import type { BunRequest } from "bun";
+import {randomUUIDv7} from "bun";
 import {db} from "../db.ts";
 import {z} from "zod";
+import type {AuthDetails} from "../utils/authenticationUtils.ts";
 
-export function getPrintQueue(req: BunRequest): Response {
+export function getPrintQueue(req: BunRequest, authDetails: AuthDetails): Response {
     const selectPrintQueueStatement = db.query("SELECT * FROM print_queue");
     const printQueue = selectPrintQueueStatement.all();
     return Response.json(printQueue);
@@ -13,9 +15,18 @@ const PrintItemSchema = z.object({
     url: z.string().nullable(),
 });
 
-export async function postPrintQueue(req: BunRequest): Promise<Response> {
+export async function postPrintQueue(req: BunRequest, authDetails: AuthDetails): Promise<Response> {
     const body = PrintItemSchema.parse(await req.json());
-    const insertPrintQueueStatement = db.query("INSERT INTO print_queue (name, url) VALUES ($name, $url) RETURNING *");
-    const created = insertPrintQueueStatement.get(body);
+    const insertPrintQueueStatement = db.query(
+        "INSERT INTO print_queue (uuid, name, url, status, status_updated_by, created_by) VALUES ($uuid, $name, $url, $status, $status_updated_by, $created_by) RETURNING *"
+    );
+    const created = insertPrintQueueStatement.get({
+        uuid: randomUUIDv7(),
+        name: body.name,
+        url: body.url,
+        status: "pending",
+        status_updated_by: authDetails.userUuid,
+        created_by: authDetails.userUuid,
+    });
     return Response.json(created);
 }
---
+++ frontend/src/hooks/useUser.tsx
@@ -0,0 +1,25 @@
+import {createContext, ReactElement, useContext} from "react";
+import {useQuery} from "@tanstack/react-query";
+import {requestHeaders} from "../queryClient.ts";
+
+type PrintFlowUser = {
+    userUuid: string;
+}
+
+const UserContext = createContext<PrintFlowUser | null>(null);
+export const useUser = () => {
+    const user = useContext(UserContext);
+    return user;
+};
+
+export const UserContextProvider = ({children}: { children: string | ReactElement | ReactElement[] }) => {
+    const {data} = useQuery({
+        queryKey: ['user'],
+        queryFn: () => fetch('/api/user', {
+            headers: requestHeaders
+        }).then(res => res.json<PrintFlowUser>()),
+    })
+    console.log(data);
+
+    return <UserContext.Provider value={data}>{children}</UserContext.Provider>;
+}
