058e3c4b540fc43982693c13269f2e5e700eb6c3
ejoneid <eirijomine@hotmail.com>
2025-03-29 15:20:52 +0100

    chore(r): sqlite volume

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,3 +1,4 @@
 import { Database } from "bun:sqlite";
 
-export const db = new Database("print-flow.sqlite", { strict: true, create: true });
+const DB_FILE = process.env.DB_LOCATION ?? "print-flow.sqlite";
+export const db = new Database(DB_FILE, { strict: true, create: true });
