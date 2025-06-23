1d008b078d5a4c965af8782c156c906f74bb7020
ejoneid <eirijomine@hotmail.com>
2025-06-23 22:10:18 +0200

    feat(b,f): model images
    model images for makerworld and printables

--- backend/src/user/userService.ts
+++ backend/src/user/userService.ts
@@ -1,9 +1,9 @@
-import type {AuthDetails} from "../security/withAuthentication.ts";
-import type {BunRequest} from "bun";
-import type {PrintFlowUser, UserMetaData} from "shared/browser";
-import {logger} from "shared/node";
-import {TEST_USERS} from "./testUsers.ts";
-import {getUserMetadata} from "supertokens-node/recipe/usermetadata";
+import type { AuthDetails } from "../security/withAuthentication.ts";
+import type { BunRequest } from "bun";
+import type { PrintFlowUser, UserMetaData } from "shared/browser";
+import { logger } from "shared/node";
+import { TEST_USERS } from "./testUsers.ts";
+import { getUserMetadata } from "supertokens-node/recipe/usermetadata";
 import supertokens from "supertokens-node";
 import Session from "supertokens-node/recipe/session";
 import UserMetadata from "supertokens-node/recipe/usermetadata";
@@ -19,13 +19,9 @@ if (process.env.ALLOW_AUTH_OVERRIDE !== "true") {
       apiDomain: "http://localhost:8000",
       websiteDomain: "http://localhost:3000",
     },
-    recipeList: [
-      Session.init(),
-      UserMetadata.init(),
-    ],
+    recipeList: [Session.init(), UserMetadata.init()],
   });
 }
-
 
 export const getUser = (req: BunRequest, authDetails: AuthDetails): Response => {
   return Response.json({
@@ -40,26 +36,28 @@ export const getUser = (req: BunRequest, authDetails: AuthDetails): Response => 
 
 export async function getUserMetaDataById(userId: UUID): Promise<UserMetaData> {
   if (process.env.ALLOW_AUTH_OVERRIDE === "true") {
-    const user = TEST_USERS[userId]
+    const user = TEST_USERS[userId];
     return {
       fullName: user.fullName,
-      avatar: user.avatar
-    }
+      avatar: user.avatar,
+    };
   }
   return (await getUserMetadata(userId)).metadata;
 }
 
 export async function getUserMetaDataByIds(userIds: UUID[]) {
   const uniqueUserIds = Array.from(new Set(userIds));
-  const metadataPromises: Promise<[UUID, UserMetaData]>[] = uniqueUserIds.map(async (userId): Promise<[UUID, UserMetaData]> => {
-    try {
-      const metadata = await getUserMetaDataById(userId);
-      return [userId, metadata];
-    } catch (error) {
-      logger.error(`Error fetching metadata for user ${userId}:`, error);
-      throw error
-    }
-  });
+  const metadataPromises: Promise<[UUID, UserMetaData]>[] = uniqueUserIds.map(
+    async (userId): Promise<[UUID, UserMetaData]> => {
+      try {
+        const metadata = await getUserMetaDataById(userId);
+        return [userId, metadata];
+      } catch (error) {
+        logger.error(`Error fetching metadata for user ${userId}:`, error);
+        throw error;
+      }
+    },
+  );
 
   const results = await Promise.all(metadataPromises);
   return new Map(results);
